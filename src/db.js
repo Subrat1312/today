8ddb5265b1e6d948d1c640010690a3dd6e8632b5
Subrat1312 <101416543+Subrat1312@users.noreply.github.com>
2022-12-04 07:46:42 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const { connect } = require("mongoose")
 
 const connectDb = async () => {
-  return connect("mongodb+srv://admin:Subrat12@cluster0.qchimot.mongodb.net/?retryWrites=true&w=majority", { dbName: process.env.DB_NAME })
+  return connect("mongodb+srv://admin:Subrat12@cluster0.qchimot.mongodb.net/?retryWrites=true&w=majority/BookManagement", { useNewUrlParser: true })
 }
 
 
